84b1cf77faabe600211aab9c966b7fafe1c1031c
prakas-surya <simplesuryaprakash123@gmail.com>
2022-03-04 12:58:15 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+const mongoose=require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+
+app.use(bodyParser.json());
+
+//import Routes 
+const postRoute = require('./routes/posts');
+
+app.use('/posts', postRoute);
+
+//Routes
+app.get('/',(req,res)=> {
+    res.send('we are on home');
+});   
+
+
+
+//connect to database
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true},
+    ()=> console.log('connected to DB!!')
+);
+
+
+//server listen
+app.listen(3000);
